3a372a501b61472697304caa8da00cb528b87144
Daniel Hazelton <madman@keil-draco.com>
2014-05-22 18:10:14 -0400

    test.js was not supposed to be added to the repo

--- src/test.js
+++
@@ -1,8 +0,0 @@
-var console = require('console');
-var cnf = require('./config.js');
-//var conf = new cnf( { 'file' : "settings01.yaml", 'path' : "/etc" } );
-var conf = new cnf();
-
-console.log( conf.getValue('blargh') );
-conf.setValue( 'blargh', true );
-console.log( conf.getValue('blargh') );
